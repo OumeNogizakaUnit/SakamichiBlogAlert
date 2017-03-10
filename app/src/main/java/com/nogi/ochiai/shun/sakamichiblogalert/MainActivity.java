c248bdcdfd0b38e86ddb7c4216286582f086937d
shun <shun@isasaka.jp>
2017-03-10 20:19:51 +0900

    Add OnClickListeiner in MainActivity
    mainのボタンに画面遷移のクラスを実装

--- app/src/main/java/com/nogi/ochiai/shun/sakamichiblogalert/MainActivity.java
+++ app/src/main/java/com/nogi/ochiai/shun/sakamichiblogalert/MainActivity.java
@@ -1,7 +1,9 @@
 package com.nogi.ochiai.shun.sakamichiblogalert;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a;
+
+        View.OnClickListener searchClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClassName("com.nogi.ochiai.shun.sakamichiblogalert","com.nogi.ochiai.shun.sakamichiblogalert.SearchActivity");
+
+                startActivity(intent);
+            }
+        };
+        this.findViewById(R.id.search).setOnClickListener(searchClickListener);
+
+        View.OnClickListener settingClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClassName("com.nogi.ochiai.shun.sakamichiblogalert","com.nogi.ochiai.shun.sakamichiblogalert.SettingActivity");
+
+                startActivity(intent);
+            }
+        };
+        this.findViewById(R.id.setting).setOnClickListener(settingClickListener);
     }
 }
