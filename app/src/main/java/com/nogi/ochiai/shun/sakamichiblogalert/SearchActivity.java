e7a6123db42d18858bcc1259ca90e7222e44c4d8
shun <shun@isasaka.jp>
2017-03-11 03:11:24 +0900

    Add Browser
    ブラウザの追加
    追加に応じてINTERNETのパーミッションも追加

--- app/src/main/java/com/nogi/ochiai/shun/sakamichiblogalert/SearchActivity.java
+++ app/src/main/java/com/nogi/ochiai/shun/sakamichiblogalert/SearchActivity.java
@@ -1,7 +1,13 @@
 package com.nogi.ochiai.shun.sakamichiblogalert;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.EditText;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +15,31 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        final EditText editText = (EditText)this.findViewById(R.id.search_word);
+
+        View.OnClickListener searchResultClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClassName("com.nogi.ochiai.shun.sakamichiblogalert","com.nogi.ochiai.shun.sakamichiblogalert.SearchResultActivity");
+
+                String searchWord = editText.getText().toString();
+                Log.d("SearchActivity.onCreate","searchWord: " + searchWord);
+                intent.putExtra("com.nogi.ochiai.shun.sakamichiblogalert.searchWords", searchWord);
+
+                startActivity(intent);
+            }
+        };
+        this.findViewById(R.id.search_result).setOnClickListener(searchResultClickListener);
+
+        View.OnClickListener browserClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://blog.nogizaka46.com/third/2017/03/037256.php"));
+                startActivityForResult(intent, 0);
+            }
+        };
+        this.findViewById(R.id.browser).setOnClickListener(browserClickListener);
     }
 }
--- app/src/main/java/com/nogi/ochiai/shun/sakamichiblogalert/SearchResultActivity.java
+++ app/src/main/java/com/nogi/ochiai/shun/sakamichiblogalert/SearchResultActivity.java
@@ -1,7 +1,11 @@
 package com.nogi.ochiai.shun.sakamichiblogalert;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 public class SearchResultActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class SearchResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+
+        // 値の受取
+        Intent intent = getIntent();
+        String searchWord = intent.getStringExtra("com.nogi.ochiai.shun.sakamichiblogalert.searchWords");
+        TextView textView = (TextView)this.findViewById(R.id.search_word_result);
+        textView.setText(searchWord);
+
+        WebView webView = (WebView) this.findViewById(R.id.webview);
+        //リンクをタップしたときに標準ブラウザを起動させない
+        webView.setWebViewClient(new WebViewClient());
+
+        //最初にgoogleのページを表示する。
+        webView.loadUrl("http://blog.nogizaka46.com/third/2017/03/037256.php");
+
+        //jacascriptを許可する
+        webView.getSettings().setJavaScriptEnabled(true);
     }
 }
