35e18f23cee1b431ae57f0167ee58541d00e15e6
shun <shun@isasaka.jp>
2017-03-10 18:20:39 +0900

    Add Search SearchResult Setting Activity
    SearchActivity
    SearchResultActivity
    SettingActivityの追加

---
+++ app/src/main/java/com/nogi/ochiai/shun/sakamichiblogalert/SearchResultActivity.java
@@ -0,0 +1,12 @@
+package com.nogi.ochiai.shun.sakamichiblogalert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
