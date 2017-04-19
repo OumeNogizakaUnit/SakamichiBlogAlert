6f7ce2f378889bdff977cdedc29f08de790cc9aa
GitHub <noreply@github.com>
2017-04-20 00:11:22 +0900

    blogAPIへのアクセスを実装
    非同期のHTTP通信でblogAPIにクエリを投げる関数を作成
    blogAPI側が落ちてしまい結果は未検証
    出力はLogのみ

--- app/src/main/java/com/nogi/ochiai/shun/sakamichiblogalert/MainActivity.java
+++ app/src/main/java/com/nogi/ochiai/shun/sakamichiblogalert/MainActivity.java
@@ -1,9 +1,20 @@
 package com.nogi.ochiai.shun.sakamichiblogalert;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getRequest("select id,auth,title,datestr from entry limit 100");
         View.OnClickListener searchClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,5 +45,41 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         this.findViewById(R.id.setting).setOnClickListener(settingClickListener);
+    }
+    public void getRequest(final String query) {
+
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String result = null;
+                // リクエストボディを作る
+                RequestBody requestBody = new FormBody.Builder()
+                        .add("query", query)
+                        .build();
+                // リクエストオブジェクトを作って
+                Request request = new Request.Builder()
+                        .url("http://terada-ranze.mydns.jp/blogAPI.php")
+                        .post(requestBody)
+                        .build();
+
+                // クライアントオブジェクトを作って
+                OkHttpClient client = new OkHttpClient();
+
+                // リクエストして結果を受け取って
+                try {
+                    Response response = client.newCall(request).execute();
+                    result = response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // 返す
+                return result;
+            }
+            @Override
+            protected void onPostExecute(String result) {
+                Log.d("Async", result);
+            }
+        }.execute();
     }
 }
